fd0951073f11d10f8e62cd3cc4b63af4a1ed8b9d
kmcclendon <kmcclendon56@gmail.com>
2022-07-22 15:21:35 -0500

    Add constants, state vairables, caches, event listeners, and functions

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*----- constants -----*/
+const food;
+const toy;
+const bed;
+const coat;
+const fan;
+
+
+/*----- app's state (variables) -----*/
+let score = 0;
+let win;
+let lose;
+/*----- cached element references -----*/
+const scoreEl = document.querySelector('.scoreBox');
+const xEl = document.querySelector('.Xs');
+
+/*----- event listeners -----*/
+document.querySelector('button')
+    .addEventListener('click', needMet);
+
+/*----- functions -----*/
+//initial function for start of game
+function init(){
+
+}
+
+//function for meeting needs
+function needMet(){
+
+}
+
+//render functions
+//winner function will show a gif of fireworks over screen
+function renderWinner(){
+
+}
+
+//loss function will show gif of rip over screen
