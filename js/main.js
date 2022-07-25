cd8499118984be049939bf33ccdbbc073808c0f8
kmcclendon <kmcclendon56@gmail.com>
2022-07-25 18:32:56 -0500

    Fix gifs and functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 /*----- constants -----*/
-const needs = ["food", "toy", "bed", "coat", "fan"];
+const needs = ["food", "a toy", "a bed", "a coat", "a fan"];
+const xArray = ["X", "X", "X"];
 
 /*----- app's state (variables) -----*/
 let score = 0;
@@ -11,7 +12,13 @@ let currentNeed;
 
 /*----- cached element references -----*/
 const scoreEl = document.querySelector('.scoreBox');
-const xEl = document.querySelector('.Xs');
+const xEl = document.getElementById('xs');
+const boredGifEl = document.querySelector('.bored');
+const sleepGifEl = document.querySelector('.sleep');
+const fireGifEl = document.querySelector('.fire');
+const snowGifEl = document.querySelector('.snow');
+const hungerGifEl = document.querySelector('.hunger');
+
 
 
 /*----- event listeners -----*/
@@ -45,92 +52,145 @@ function init(){
     //set initial values
     score = 0;
     winner = null;
-    currentNeed = randomNeed();
+    randomNeed();
     console.log(`Your friend needs ${currentNeed}!`);
+
     render()
 }
 
 function render(){
-    // Show need?
     scoreEl.innerText = score;
 }
 
 function randomNeed() {
    let needIndex = Math.floor(Math.random() * 5);
-    return needs[needIndex];
+   currentNeed = needs[needIndex];
+   if(currentNeed === "food"){
+    showHungerGif();
+    }
+    else if(currentNeed === "a bed"){
+        showSleepGif();
+    }
+    else if(currentNeed === "a toy"){
+        showBoredGif();
+    }
+    else if(currentNeed === "a coat"){
+        showSnowGif();
+    }
+    else{
+        showFireGif();
+    }    
 }
 
 function plusScore(){
     if(score === 3){
-        renderWinner()
+        renderWinner();
     }
     else{
         score += 1;
-        currentNeed = randomNeed();
+        randomNeed();
+        console.log(`Your friend needs ${currentNeed}!`);
     }
     render()
 }
 
 function minusScore(){
     if(xCount === 3){
-        renderLoss()
+        renderLoss();
     }
     else{
         xCount += 1;
-        currentNeed = randomNeed();
+        //xEl.style.color = "red";
+        randomNeed();
     }
     render()
 }
 
-//function for meeting needs
+//Show hidden gifs functions
+function showHungerGif() {
+    hungerGifEl.style.visibility = 'visible';
+    console.log("Hunger");
+}
+function showSnowGif() {
+    snowGifEl.style.visibility = 'visible';
+    console.log("Cold");
+}
+function showFireGif() {
+    fireGifEl.style.visibility = 'visible';
+    console.log("Hot");
+}
+function showSleepGif() {
+    sleepGifEl.style.visibility = 'visible';
+    console.log("Tired");
+}
+function showBoredGif() {
+    boredGifEl.style.visibility = 'visible';
+    console.log("Bored");
+}
+
+
+//functions for meeting needs
 function foodNeedMet(){
-    // or needs[0]
     if(currentNeed === "food"){
         plusScore();
+        hungerGifEl.style.visibility = 'hidden';
     } else {
-        minusScore()
+        minusScore();
     }
+    render()
 }
 
 function toyNeedMet(){
-    if(currentNeed === "toy"){
+    if(currentNeed === "a toy"){
         plusScore();
+        boredGifEl.style.visibility = 'hidden';
     } else {
-        minusScore()
+        minusScore();
     }
+    render()
+
 }
 
 function bedNeedMet(){
-    if(currentNeed === "bed"){
+    if(currentNeed === "a bed"){
         plusScore();
+        sleepGifEl.style.visibility = 'hidden';
     } else {
-        minusScore()
+        minusScore();
     }
+    render()
+
 }
 
 function coatNeedMet(){
-    if(currentNeed === "coat"){
+    if(currentNeed === "a coat"){
         plusScore();
+        snowGifEl.style.visibility = 'hidden';
     } else {
-        minusScore()
+        minusScore();
     }
+    render()
+
 }
 
 function fanNeedMet(){
-    if(currentNeed === "fan"){
+    if(currentNeed === "a fan"){
         plusScore();
+        fireGifEl.style.visibility = 'hidden';
     } else {
-        minusScore()
+        minusScore();
     }
+    render()
+
 }
 
 function replay(){
-    init();
+    //init();
+    randomNeed();
 }
 
 //render functions
 //winner function will show a gif of fireworks over screen
-//
 function renderWinner(){
 
 }
